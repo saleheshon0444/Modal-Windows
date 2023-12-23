99203ece05830c8072681f2a84c966fcde7b986f
Saleh <m.saleh0444@gmail.com>
2023-12-23 11:22:29 +0330

    completing

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-'use strict';
+"use strict";
+const documentShow_Modal = document.querySelectorAll(".show-modal");
+const documentModal = document.querySelector(".modal");
+const documentOverlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const modalFunc = function () {
+  documentModal.classList.toggle("hidden");
+  documentOverlay.classList.toggle("hidden");
+};
+
+for (let i = 0; i < documentShow_Modal.length; i++)
+  documentShow_Modal[i].addEventListener("click", modalFunc);
+
+btnCloseModal.addEventListener("click", modalFunc);
+documentOverlay.addEventListener("click", modalFunc);
+
+document.addEventListener('keydown', function(event){
+    console.log(event.key);
+    event.key == 'Escape' && !documentModal.classList.contains('hidden')?
+    modalFunc(): console.log(); 
+    
+})
 
